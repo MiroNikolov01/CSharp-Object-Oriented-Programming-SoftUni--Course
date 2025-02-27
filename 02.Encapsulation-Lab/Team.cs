8c2c7368b542778ff78e45733cbed41373af5f18
Stanimir_Nikolov <166949863+MiroNikolov01@users.noreply.github.com>
2025-02-27 12:18:08 +0200

    Add files via upload

---
+++ 02.Encapsulation-Lab/Team.cs
@@ -0,0 +1,30 @@
+﻿namespace PersonsInfo;
+
+public class Team
+{
+    private readonly string name;
+    private readonly List<Person> firstTeam;
+    private readonly List<Person> reserveTeam;
+
+    public string Name => name;
+    public IReadOnlyCollection<Person> FirstTeam { get; }
+    public IReadOnlyCollection<Person> ReserveTeam { get; }
+
+    public Team(string name)
+    {
+        this.name = name;
+        firstTeam = new List<Person>();
+        FirstTeam = firstTeam.AsReadOnly();
+        
+        reserveTeam = new List<Person>();
+        ReserveTeam = reserveTeam.AsReadOnly();
+    }
+
+    public void AddPlayer(Person person)
+    {
+        if(person is null) throw new ArgumentNullException(nameof(person));
+        
+        if(person.Age < 40) firstTeam.Add(person);
+        else reserveTeam.Add(person);
+    }
+}
